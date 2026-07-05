30b252a69218ef41931c8687328b9c3f3dba4e00
Umerrazakamran-Developer <umerrazakamran@gmail.com>
2026-07-06 01:01:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 10; i++) {
+    console.log("2 X " + i + " = " + 2 * i);
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(" 3 X " + i + " = " + 3 * i);
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(" 4 X " + i + " = " + 4 * i);
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(" 5 X " + i + " = " + 5 * i);
+}
