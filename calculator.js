67bf321246e165d3d93ccbf86b845e93f6ec857b
Umerrazakamran-Developer <umerrazakamran@gmail.com>
2026-07-06 01:00:00 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,26 @@
+let num1 = 11;
+let num2 = 5; 
+let operator = "+";
+
+if (operator === "+") {
+    console.log(num1 + num2);
+} 
+else if (operator === "-") {
+    console.log(num1 - num2);
+}
+else if (operator === "*") {
+    console.log(num1 * num2);
+}
+else if (operator === "/") {
+    console.log(num1 / num2);
+}
+else if (operator === "%") {
+    console.log(num1 % num2);
+}
+else if (operator === "**") {
+    console.log(num1 ** num2);
+}
+else {
+    console.log("Wrong Operator!");
+}
+
